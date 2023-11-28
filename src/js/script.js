735f509e3f7e28fbd861271497d47da2e9f9250a
Gabriela Rodrigues <gabrielarodrigues502@hotmail.com>
2023-11-28 20:33:56 -0300

    finalização das paginas de contato, sobre, index e voluntarios, implementação do js para pop up de termos de uso de imagem do voluntario

--- src/js/script.js
+++ src/js/script.js
@@ -36,3 +36,38 @@ slider.addEventListener("mousemove", (e) => {
   const walk = (x - startX) * SCROLL_SPEED;
   slider.scrollLeft = scrollLeft - walk;
 });
+
+//função do checkbox do pop-up de termos de uso para ser voluntario
+function handleShowPopUp() {
+  var popup = document.getElementById('pop_up')
+
+  popup.style.display = 'flex'
+}
+
+function handleHidePopUp() {
+  var popup = document.getElementById('pop_up')
+  var checkbox = document.getElementById('checkbox')
+
+  checkbox.checked = true
+
+  popup.style.display = 'none'
+}
+
+//função que vai redirecionar para as redes sociais
+
+function redirectFacebook (){
+  window.location.href='https://www.facebook.com/profile.php?id=100089227661691&mibextid=LQQJ4d'
+}
+
+function redirectInstagram(){
+  window.location.href='https://instagram.com/todasportodos?igshid=MzMyNGUyNmU2YQ=='
+}
+
+function redirectTikTok(){
+  window.location.href='https://www.tiktok.com/@todasportodos?_t=8hkNz29Jazt&_r=1'
+}
+
+function redirectWhatsapp(){
+  window.location.href='https://chat.whatsapp.com/KKxqivG2NCUG9W3FLQvHhB'
+}
+
