aa02f52118e2eb2f6437f641a0246f7e4ffa02be
Buletiss <vinicius1997feitosa@gmail.com>
2023-11-28 23:19:22 -0300

    adição dos inputs na aba de doação e criação do script para donate

--- src/js/script.js
+++ src/js/script.js
@@ -36,3 +36,40 @@ slider.addEventListener("mousemove", (e) => {
   const walk = (x - startX) * SCROLL_SPEED;
   slider.scrollLeft = scrollLeft - walk;
 });
+
+function donateProcess() {
+  var valor = document.getElementById("valor").value;
+  var nome = document.getElementById("nome").value;
+  var email = document.getElementById("email").value;
+
+  if (valor <= 0 || email.trim() === "") {
+    alert("Por favor, preencha todos os campos corretamente.");
+    return;
+  }
+
+  var formattedValue = parseFloat(valor).toLocaleString("pt-BR", {
+    minimumFractionDigits: 2,
+  });
+
+  var data = {
+    valor: `R$${formattedValue}`, // foi feito desta maneira porque o tolocalestring estava devolvendo caracteres indesejados
+    nome,
+    email,
+  };
+
+  // Conf AJAX
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://enqvbcorviyeb.x.pipedream.net", true); // usar o site requestbin e trocar a url para receber os dados
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      alert("Obrigado por sua doação, " + nome + "!");
+      document.getElementById("doacaoForm").reset();
+    } else {
+      console.error("Erro ao processar a doação:", xhr.statusText);
+    }
+  };
+
+  xhr.send(JSON.stringify(data));
+}
